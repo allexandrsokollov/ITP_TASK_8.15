b566bc53228a01ac076624806ad5c54a40e6d6ab
alexander <allexandersokollov@gmail.com>
2021-12-12 10:33:47 +0300

    file out has been written not tested

--- src/main/java/cs/vsu/sokolov/fileHandler/fileWriter.java
+++ src/main/java/cs/vsu/sokolov/fileHandler/fileWriter.java
@@ -1,4 +1,32 @@
 package cs.vsu.sokolov.fileHandler;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class fileWriter {
+
+    private final String filePath;
+
+    public fileWriter(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void writeTwoDimensIntArr (int[][] ints) throws IOException {
+
+        try (FileOutputStream out = new FileOutputStream(filePath)) {
+
+            for (int[] anInt : ints) {
+                for (int i : anInt) {
+                    out.write((byte) i);
+                    out.write((byte) ' ');
+                }
+                out.write((byte) '\n');
+            }
+        }
+
+        System.out.printf("Array has been written to file %s", filePath);
+    }
+
+
 }
+
